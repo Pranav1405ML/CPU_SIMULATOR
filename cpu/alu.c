4d39ae0a055cdbf010b12128e67c63753fb8f0d2
Pranav Ganesh <pranavganesh1405@gmail.com>
2026-09-06 18:17:27 +0530

    Fix ALU carry detection using uint16 widening instead of fragile bitwise loop

--- cpu/alu.c
+++ cpu/alu.c
@@ -1,15 +1,24 @@
 #include "machine.h"
 
 struct ALU_result add_8bit(uint8_t a, uint8_t b){
+    uint16_t sum = (uint16_t)a + (uint16_t)b;
     struct ALU_result ALU = {0};
-    uint8_t carry;
-    while(b != 0){
-        carry = a & b;           // Carry
-        a = a ^ b;               // Sum
-        if((carry & (1 << 7)) != 0) ALU.C_flag = true;
-        b = carry << 1;          // add carry to next digit place 
-    }
-    ALU.result = a;
-    if(a == 0) ALU.Z_flag = true;
+    ALU.result = (uint8_t)sum;
+    ALU.C_flag = (sum > 255);
+    ALU.Z_flag = (ALU.result == 0);
     return ALU;
 }
+
+// struct ALU_result add_8bit(uint8_t a, uint8_t b){
+//     struct ALU_result ALU = {0};
+//     uint8_t carry;
+//     while(b != 0){
+//         carry = a & b;           // Carry
+//         a = a ^ b;               // Sum
+//         if((carry & (1 << 7)) != 0) ALU.C_flag = true;
+//         b = carry << 1;          // add carry to next digit place 
+//     }
+//     ALU.result = a;
+//     if(a == 0) ALU.Z_flag = true;
+//     return ALU;
+// }
